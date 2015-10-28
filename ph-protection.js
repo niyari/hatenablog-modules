3c25dfc159b59cf103cbf5378dd3b7b73f585ea8
niyari <niyari@users.noreply.github.com>
2015-10-28 21:59:39 +0900

    Proxy Hacking Protection の設定周りの変更
    PC版/スマホ版といった複数のURLを判定できるように変更した

--- ph-protection.js
+++ ph-protection.js
@@ -11,22 +11,27 @@
  * 「( ・ω・)┌*━━━ Laser Beam!!!! https://www.youtube.com/watch?v=K54CYowOqxM
  */
 (function () {
-	var setting = {};
+	var setting = [];
 	function siteCheck() {
-		console.log(setting);
-		var pattern = setting.baseURI;
-		if (pattern.indexOf(location.hostname) < 0) {
-			insertTag(pattern);
-		} else if (window != parent) {
-			insertTag(pattern);
-		} else {
-			//console.log("正しいと思われます");
+		var settingData = '';
+		for (i = 0; i < setting.length ; i++) {
+			var pattern = setting[i].hostName;
+			var target = location.hostname;
+			if (window != parent) target = document.referrer.split('/')[2];
+			if (pattern.indexOf(target) !== 0) {
+				settingData = settingData === '' ? setting[i] : settingData;
+			} else {
+				settingData = '';
+				break;
+				//console.log("正しいと思われます");
+			}
 		}
+		if (settingData) insertTag(settingData);
 	}
 	function setupSiteCheck(que) {
 		if (que instanceof Array === false) {
 			var que = [];
-			//que.push(['setting', {'baseURI':'http://psn.hatenablog.jp/'}]);
+			//que.push(['setting', [{'uriScheme':'http','hostName':'psn.hatenablog.jp'}]]);
 		}
 		for (var i = 0; i < que.length; i++) {
 			switch (que[i][0]) {
@@ -35,40 +40,34 @@
 					break;
 			}
 		}
-		setting.ogURL = getOGP('og:url');
-		if (!setting.baseURI && !setting.ogURL) {
+		if (!setting[0].hostName) {
 			console.log('設定がありません');
 			return;
-		}
-		if (!setting.baseURI && setting.ogURL) setting.baseURI = setting.ogURL;
-		if (setting.uriScheme && setting.hostName) {
-			if ((setting.uriScheme + '://' + setting.hostName) !== setting.baseURI) {
-				setting.baseURI = setting.uriScheme + '://' + setting.hostName; // アドレス書き換え系の対策
-			}
-			// TODO: URL書き換え系の場合はcanonicalも変わっている可能性がある為、データを正してやる必要がある
 		}
 		siteCheck();
 	}
 	function getOGP(prop) {
 		var metaTag = document.getElementsByTagName("meta");
-		for (i = 0; i < metaTag.length; i++) if (metaTag[i].getAttribute("property") == prop) return metaTag[i].getAttribute("content")
+		for (i = 0; i < metaTag.length; i++)
+			if (metaTag[i].getAttribute("property") == prop) return metaTag[i].getAttribute("content")
 	}
-
-	function insertTag(pattern) {
+	function getCanonical() {
+		for (var linkTag = document.getElementsByTagName("link"), a = 0; a < linkTag.length; a++)
+			if ("canonical" === linkTag[a].getAttribute("rel")) return linkTag[a].getAttribute("href")
+	};
+	function targetURI(settingData) {
+		var canonical = getCanonical().split(settingData.hostName)[1];
+		if (!canonical) canonical = getOGP('og:url').split(settingData.hostName)[1];
+		if (!canonical) canonical = '';
+		return settingData.uriScheme + '://' + settingData.hostName + canonical;
+	}
+	function insertTag(settingData) {
 		var head_tag = document.createElement("meta");
 		head_tag.name = "robots";
 		head_tag.content = "noindex";
 		document.getElementsByTagName("head")[0].appendChild(head_tag);
-		var canonical = "";
-		var links = document.getElementsByTagName("link");
-		for (var i = 0; i < links.length; i++) {
-			if (links[i].getAttribute("rel") === "canonical") {
-				canonical = links[i].getAttribute("href")
-			}
-		}
-		var targetURI = canonical.indexOf(pattern) === 0 ? canonical : pattern;
 		var error_box = document.createElement("div");
-		error_box.innerHTML = '<a href="' + targetURI + '" target="_top">'
+		error_box.innerHTML = '<a href="' + targetURI(settingData) + '" target="_top">'
 			+ '<div class="error-box" style="position: fixed; top: 40px; left: 30px; font-size: 1.5em; z-index: 800000;'
 			+ ' line-height: 1.1; color: #3A3A3A; padding: 0.8em; margin: 0.5em 0 1em; background-color: rgba(249, 234, 234, 0.85); border-left: 5px solid #E83731;">'
 			+ 'このページはキャッシュされている可能性があります。<br>記事の最新版はこちらから</div></a>';
